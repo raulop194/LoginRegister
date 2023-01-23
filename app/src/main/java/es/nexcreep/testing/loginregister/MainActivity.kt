aeb4fa58dc2e06746c0f2f4805c3c0dd2edae813
raulop194 <raulop194@alu.centroafuera.es>
2023-01-23 11:10:46 +0100

    Initial Commit

---
+++ app/src/main/java/es/nexcreep/testing/loginregister/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.nexcreep.testing.loginregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
