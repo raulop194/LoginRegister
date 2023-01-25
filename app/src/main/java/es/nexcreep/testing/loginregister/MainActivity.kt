6df98c947b57e5af0a73c15fe74e35fd877d1bfc
raulop194 <raulop194@alu.centroafuera.es>
2023-01-25 10:35:36 +0100

    La mia XDDDDDDDDDDDDDDDDDDDDD

--- app/src/main/java/es/nexcreep/testing/loginregister/MainActivity.kt
+++ app/src/main/java/es/nexcreep/testing/loginregister/MainActivity.kt
@@ -1,11 +1,27 @@
 package es.nexcreep.testing.loginregister
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import es.nexcreep.testing.loginregister.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private var loggedIn: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loggedIn = intent.getBooleanExtra("LOGGED_IN", false)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!loggedIn)
+            startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
--- app/src/main/java/es/nexcreep/testing/loginregister/RegisterActivity.kt
+++ app/src/main/java/es/nexcreep/testing/loginregister/RegisterActivity.kt
@@ -1,11 +1,41 @@
 package es.nexcreep.testing.loginregister
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import es.nexcreep.testing.loginregister.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.nacionalidades,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        binding.nacionalidadesSpinner.adapter = adapter
+
+        binding.button3.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+                    .putExtra("LOGGED_IN", true)
+            )
+        }
+
+        binding.button2.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }
+
     }
 }
